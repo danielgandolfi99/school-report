03fe6a0000f271cb8b048a708de6f8abb3876f6c
Daniel Gandolfi <danielgandolfi.10@gmail.com>
2024-11-24 22:21:14 -0300

    fix: ajuste no axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 // next
 import { getSession } from 'next-auth/react';
 
-const axiosServices = axios.create({ baseURL: process.env.NEXT_APP_API_URL || 'http://localhost:3010/' });
+const axiosServices = axios.create({ baseURL: 'http://localhost:8081/' });
 
 // ==============================|| AXIOS - FOR MOCK SERVICES ||============================== //
 
